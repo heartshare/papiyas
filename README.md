0fd83714ad52c77481342c7c0d170fbe3a55fd32
papiyas <13566965652@163.com>
2020-12-18 01:46:54 +0800

    添加几个常用命令

---
+++ README.md
@@ -0,0 +1,19 @@
+# 目前命令如下
+
+如果对命令不了解如何使用, 可以使用-h|--help指令查看, 例如`./bin/papiyas server:start -h`
+
+## server
+
++ server:start
++ server:stop
++ server:restart
++ server:remove
++ server:ps
+
+
+## 其他命令
+
++ docker-compose  
++ dc
++ php
++ npm
--- controllers/MainController.sh
+++ controllers/MainController.sh
@@ -2,11 +2,74 @@
 
 
 main::configure() {
-    add_argument 'name' $INPUT_ARRAY
+    add_option 'u' 'user' $OPTION_OPTIONAL '容器操作的用户名' 'laradock'
 }
 
 
+################################################################
+## docker-compose
+## @alias: 可使用简写命令dc代替
+## @description: 直接调用docker-compose执行原生指令
+## @memo: 由于是全局调用, --project-directory指向了laradock目录
+##   -f 指向了配置文件中的compose_file.
+## 
+################################################################
+main::docker-compose() {
+  docker_compose "${params[@]}"
+}
+
+################################################################
+## dc
+## @alias: docker-compose的简写
+## @description: 直接调用docker-compose执行原生指令
+## @memo: 由于是全局调用, --project-directory指向了laradock目录
+##   -f 指向了配置文件中的compose_file.
+## 
+################################################################
+main::dc() {
+  main::docker-compose
+}
+
+
+################################################################
+## php
+## 
+## @option user (optional)
+## @description: 
+## @memo: 
+## 
+################################################################
+main::php() {
+  docker_compose exec --user=$(get_option user) workspace php  "${params[@]}";
+}
+
+
+################################################################
+## npm
+## 
+## @option user (optional)
+## @description: 
+## @memo: 
+## 
+################################################################
+main::npm() {
+  echo docker_compose exec --user="$(get_option user)" workspace npm "${params[@]}"
+}
+
+
+################################################################
+## mysql
+## 
+## @option user (optional)
+## @description: 
+## @memo: 
+## 
+################################################################
+main::mysql() {
+  echo docker_compose exec --user=mysql mysql mysql "${params[@]}"
+}
+
 
 main::list() {
-
+  echo
 }
